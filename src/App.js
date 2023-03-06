eddb2f61dc9714be8a912bca8f87c4c50b589a4c
anftlcb@gmail.com <94384392+Eojoonhyuk@users.noreply.github.com>
2023-03-06 18:56:53 +0900

    styles 폴더 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import { Login, Home, Direct } from "./pages";
-import { createGlobalStyle } from "styled-components";
+import GlobalStyle from "./styles/GlobalStyle";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const GlobalStyle = createGlobalStyle`
-  *{
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-  body{
-    box-sizing: border-box;
-  }
-`
+
 
 const App = () => {
   return (
