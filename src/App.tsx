d3e53105ed1a962eb5dda744e31c078f3d98e68a
Eojoonhyuk <anftlcb@gmail.com>
2023-10-19 17:54:06 +0900

    개발환경 설정 변경

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,17 @@ import { Login, Home, Direct } from "./pages";
 import GlobalStyle from "./styles/GlobalStyle";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
-
 const App = () => {
   return (
     <BrowserRouter>
-      <GlobalStyle/>
+      <GlobalStyle />
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/direct" element={<Direct/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/direct" element={<Direct />}></Route>
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
-
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,36 @@
 import styled from "styled-components";
-import { SideBar, PostItem, User } from "../components"
+import { SideBar, PostItem, User } from "../components";
 
 const Home = () => {
-    return (        
-        <HomeArea>
-            <SideBar/>
-            <HomeBox>
-                <ContentsBox>
-                    <PostItem/>
-                </ContentsBox>
-                <User/>
-            </HomeBox>
-        </HomeArea>
-    )
+  return (
+    <HomeArea>
+      <SideBar />
+      <HomeBox>
+        <ContentsBox>
+          <PostItem />
+        </ContentsBox>
+        <User />
+      </HomeBox>
+    </HomeArea>
+  );
 };
 
 export default Home;
 
 const HomeArea = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
+  width: 100%;
+  height: 100vh;
+  display: flex;
 `;
 
 const HomeBox = styled.div`
-    width: calc(85% - 4rem);
-    display: flex;
-    margin: 1rem 2rem 0 2rem;
+  width: calc(85% - 4rem);
+  display: flex;
+  margin: 1rem 2rem 0 2rem;
 `;
 
 const ContentsBox = styled.div`
-    width: 70%;
-    display: flex;
-    justify-content: center;
+  width: 70%;
+  display: flex;
+  justify-content: center;
 `;
